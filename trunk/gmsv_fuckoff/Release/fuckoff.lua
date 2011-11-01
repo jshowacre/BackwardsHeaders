74afc2cd90005bc8e0dc32bd68fb6f0c90441aa7
blackops7799@gmail.com <blackops7799@gmail.com@37f4889b-24e0-066a-12c2-fab583ca795a>
2011-11-01 16:20:14 +0000

    More examples/comments

--- trunk/gmsv_fuckoff/Release/fuckoff.lua
+++ trunk/gmsv_fuckoff/Release/fuckoff.lua
@@ -2,10 +2,13 @@ require("fuckoff")
 
 -- fuckoff.Drop( ply:EntIndex(), "Some reason" )
 
+-- Don't rely on this hook for a valid steamid
 hook.Add( "ClientConnect", "fuckoff1", function( name, steamid, ip, index )
 	print( "ClientConnect", name, steamid, ip, index )
+    --return false -- Deny connection
 end )
 
+-- You can rely on this hook for a valid steamid
 hook.Add( "SteamIDValidated", "fuckoff2", function( name, steamid, index )
 	print(  "SteamIDValidated", name, steamid, index )
 	return "Fuck off " .. name
