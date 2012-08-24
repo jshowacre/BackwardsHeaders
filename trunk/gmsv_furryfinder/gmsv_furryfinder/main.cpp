3f5dbe6b93a426aaad9168475933b62201e0ef3a
blackops7799 <blackops7799@37f4889b-24e0-066a-12c2-fab583ca795a>
2012-08-24 22:22:04 +0000

    Gatekeeper/Furryfinder gmod 13 builds

---
+++ trunk/gmsv_furryfinder/gmsv_furryfinder/main.cpp
@@ -0,0 +1,77 @@
+
+
+#include "FurryFinder.h"
+
+
+
+GMOD_MODULE( Load, Unload );
+
+
+
+
+LUA_FUNCTION( ReqGroupStatus )
+{
+	if ( !g_pFurryFinder )
+	{
+		Lua()->Error( "FurryFinder: pFurryFinder is NULL!\n" );
+
+		Lua()->PushNil();
+		return 1;
+	}
+
+	Lua()->CheckType( 1, Type::STRING );
+	Lua()->CheckType( 2, Type::STRING );
+
+	const char *strSteamUser = Lua()->GetString( 1 );
+	const char *strSteamGroup = Lua()->GetString( 2 );
+
+	ISteamUtils *pUtils = g_pFurryFinder->GetUtils();
+
+	CSteamID steamUser( strSteamUser, ( pUtils != NULL ? pUtils->GetConnectedUniverse() : k_EUniversePublic ) );
+
+	uint64 steamGrp64 = _atoi64( strSteamGroup );
+
+	if ( steamGrp64 == 0 )
+	{
+		Lua()->Error( "FurryFinder: invalid steam group.\n" );
+
+		Lua()->PushNil();
+		return 1;
+	}
+
+	CSteamID steamGroup( steamGrp64 );
+
+	bool bRet = g_pFurryFinder->RequestGroupStatus( steamUser, steamGroup );
+
+	Lua()->Push( bRet );
+
+	return 1;
+}
+
+LUA_FUNCTION( Load )
+{
+	if ( g_pFurryFinder )
+		delete g_pFurryFinder;
+
+	g_pFurryFinder = new CFurryFinder( L );
+
+	// try a manual init for changelevels
+	g_pFurryFinder->Init();
+
+	ILuaObject *ffTable = Lua()->GetNewTable();
+		ffTable->SetMember( "RequestGroupStatus", ReqGroupStatus );
+	Lua()->SetGlobal( "furryfinder", ffTable );
+
+	ffTable->UnReference();
+
+	return 0;
+}
+
+LUA_FUNCTION( Unload )
+{
+	if ( g_pFurryFinder )
+		delete g_pFurryFinder;
+
+	return 0;
+}
+
---
+++ trunk/gmsv_gatekeeper/src/main.cpp
@@ -0,0 +1,417 @@
+// GateKeeper V4.2
+// ComWalk, VoiDeD, Chrisaster
+#define GMOD_BETA
+
+#ifdef WIN32
+	#define VTABLE_OFFSET 0
+	#define ENGINE_LIB "engine.dll"
+
+	#define WIN32_LEAN_AND_MEAN
+	#include <windows.h>
+
+	#include "sigscan.h"
+#else if defined _LINUX
+	#define VTABLE_OFFSET 1
+	#define ENGINE_LIB "engine.so"
+
+	#include <dlfcn.h>
+	#include <sys/mman.h>
+	#include <stdlib.h>
+	#include <unistd.h>
+
+	#include "sourcemod/memutils.h"
+#endif
+
+#include "vfnhook.h"
+
+#include <tier1/tier1.h>
+#include <tier1/convar.h>
+
+#include <interface.h>
+#include <netadr.h>
+#include <inetmsghandler.h>
+#include <inetchannel.h>
+#include <steamclientpublic.h>
+#include <vstdlib/cvar.h>
+
+#include "tmpserver.h"
+#include "tmpclient.h"
+
+#include "ILuaModuleManager.h"
+
+using namespace GarrysMod::Lua;
+
+GMOD_MODULE( Load, Unload );
+
+static ConVar gk_force_protocol_enable( "gk_force_protocol_enable", "0", FCVAR_NONE, "Enable or disable gatekeeper handling of a specific network protocol." );
+static ConVar gk_force_protocol( "gk_force_protocol", "0", FCVAR_NONE, "Force gatekeeper to handle a specific protocol." );
+static ConVar gk_debug( "gk_debug", "0", FCVAR_NONE, "Enables additional debug output." );
+
+static uint64 rawSteamID = 0;
+static int clientChallenge = 0;
+
+CBaseServer* pServer = NULL;
+ILuaInterface* gLua = NULL;
+
+DEFVFUNC_( origConnectClient, void, ( CBaseServer *srv, netadr_t &netinfo, int netProt, int chal, int authProt, int challenge, const char *user, const char *pass, const char *cert, int certLen ) );
+void VFUNC newConnectClient( CBaseServer *srv, netadr_t &netinfo, int netProt, int chal, int clientchal, int authProt, const char *user, const char *pass, const char *cert, int certLen )
+{
+	clientChallenge = clientchal;
+
+	bool debugOutputEnabled = gk_debug.GetBool();
+
+	if ( debugOutputEnabled )
+	{
+		// print network and authentication protocol versions
+		Msg( "[GateKeeper] ConnectClient( netProt: %d, authProt: %d )\n", netProt, authProt );
+
+		// print client certificate
+		Msg( "[GateKeeper] Printing client (%s) certificate..\n", user );
+		for ( int i = 0; i < certLen; i++ )
+			Msg( "%02X ", (unsigned char)cert[i] );
+		Msg( "\n" );
+	}
+
+	int origNetProt = netProt;
+
+	if ( gk_force_protocol_enable.GetBool() )
+	{
+		// force a specific auth pathway
+		netProt = gk_force_protocol.GetInt();
+
+		if ( debugOutputEnabled )
+			Msg( "[GateKeeper] Forcing network protocol to %d!\n", netProt );
+	}
+
+	switch ( netProt )
+	{
+		case 14:
+		{
+			rawSteamID = *(uint64*)( cert + 16 );
+
+			break;
+		}
+		case 15:
+		case 16:
+		{
+			uint32 headerLen = *((uint32*)cert);
+
+			if ( headerLen <= 20 )
+				rawSteamID = *(uint64*)(cert + 4 + headerLen + 12);
+			else
+				rawSteamID = 0;
+
+			break;
+		}
+		case 17:
+		case 18:
+		case 19:
+		case 20:
+		case 21:
+		case 22:
+		{
+			// steamid is always at the beginning, and is reliable
+			rawSteamID = *(uint64 *)cert;
+			
+			break;
+		}
+		default:
+		{
+
+			rawSteamID = 0;
+
+			break;
+		}
+	}
+
+	if ( debugOutputEnabled )
+		Msg( "[GateKeeper] Certificate SteamID: %llu\n", rawSteamID );
+
+	return origConnectClient( srv, netinfo, origNetProt, chal, clientchal, authProt, user, pass, cert, certLen );
+}
+
+DEFVFUNC_( origCheckPassword, bool, ( CBaseServer *srv, netadr_s &adr, const char *pass, const char *user ) );
+bool VFUNC newCheckPassword( CBaseServer *srv, netadr_t &netinfo, const char *pass, const char *user )
+{
+	const char *steamid = "STEAM_ID_UNKNOWN";
+	
+	CSteamID steam( rawSteamID );
+
+	if ( steam.BIndividualAccount() )
+		steamid = steam.Render();
+	
+	ILuaObject *hookTable = gLua->GetGlobal( "hook" );
+	ILuaObject *hookCallFunc = hookTable->GetMember( "Call" );
+
+	// hook.Call
+	gLua->Push( hookCallFunc );
+	// hook name
+	gLua->Push( "PlayerPasswordAuth" );
+	// gamemode
+	gLua->PushNil();
+	// hook arguments
+	gLua->Push( user );
+	gLua->Push( pass );
+	gLua->Push( steamid );
+	gLua->Push( netinfo.ToString() );
+	// call hook
+	gLua->Call( 6, 1 );
+	// cleanup
+	hookTable->UnReference();
+	hookCallFunc->UnReference();
+
+	ILuaObject *ret = gLua->GetReturn(0);
+
+	if ( ret->GetType() == Type::BOOL )
+	{
+		bool b = ret->GetBool();
+		
+		ret->UnReference();
+
+		return b;
+	}
+	else if ( ret->GetType() == Type::STRING )
+	{
+		srv->RejectConnection( netinfo, clientChallenge, ret->GetString() );
+		
+		ret->UnReference();
+
+		return false;
+	}
+	else if ( ret->GetType() == Type::TABLE )
+	{
+		ILuaObject *allow = ret->GetMember( "1" );
+		ILuaObject *reason = ret->GetMember( "2");
+		
+		ret->UnReference();
+
+		if ( allow && allow->GetType() == Type::BOOL )
+		{
+			if ( allow->GetBool() == true )
+			{
+				return true;
+			}
+			else
+			{				
+				if ( reason != NULL )
+				{
+					if ( reason->GetType() == Type::STRING )
+					{
+						srv->RejectConnection( netinfo, clientChallenge, reason->GetString() );
+					}
+					else if ( !reason->isNil() )
+					{
+						gLua->Error( "Second return value of PlayerPasswordAuth must be nil or a string!\n" );
+					}
+				}
+
+				return false;
+			}
+		}
+	}
+	else
+	{
+		if ( !ret->isNil() )
+			gLua->Error( "PlayerPasswordAuth hook must return a boolean, string, or table value!\n" );
+		
+		ret->UnReference();
+	}
+
+	return origCheckPassword( srv, netinfo, pass, user );
+}
+
+LUA_FUNCTION( ForceProtocol )
+{
+	if ( gLua->GetType( 1 ) == Type::NIL )
+	{
+		gk_force_protocol_enable.SetValue( false );
+		gk_force_protocol.Revert();
+
+		return 0;
+	}
+
+	gLua->CheckType( 1, Type::NUMBER );
+
+	int forceProt = gLua->GetNumber( 1 );
+
+	gk_force_protocol_enable.SetValue( true );
+	gk_force_protocol.SetValue( forceProt );
+
+	return 0;
+}
+
+
+LUA_FUNCTION( GetUserByAddress )
+{
+	gLua->CheckType( 1, Type::STRING );
+	
+	const char *pszAddress = gLua->GetString( 1 );
+
+	for (int i=0; i < pServer->GetClientCount(); i++)
+	{
+		IClient *client = pServer->GetClient( i );
+
+		if( client->IsConnected() && strcmp( pszAddress, client->GetNetChannel()->GetRemoteAddress().ToString() ) == 0 )
+		{	
+			gLua->Push( (float)client->GetUserID() );
+	
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+
+LUA_FUNCTION( DropAllPlayers )
+{
+	gLua->CheckType( 1, Type::STRING );
+
+	const char *pszReason = gLua->GetString( 1 );
+
+	for (int i=0; i < pServer->GetClientCount(); i++)
+	{
+		IClient *client = pServer->GetClient( i );
+
+		if( client->IsConnected() )
+			client->Disconnect( pszReason );
+	}
+
+	return 0;
+}
+
+LUA_FUNCTION( DropPlayer )
+{
+	gLua->CheckType( 1, Type::NUMBER );
+	gLua->CheckType( 2, Type::STRING );
+
+	int iDropID = gLua->GetNumber( 1 );
+
+	for (int i=0; i < pServer->GetClientCount(); i++)
+	{
+		IClient *client = pServer->GetClient(i);
+	
+		if ( client->GetUserID() == iDropID )
+		{
+			client->Disconnect( gLua->GetString( 2 ) );
+			
+			gLua->Push( true );
+
+			return 1;
+		}
+	}
+
+	gLua->Push( false );
+
+	return 1;
+}
+
+LUA_FUNCTION( GetNumClients )
+{
+	int spawning = 0;
+	int active = 0;
+	int total = 0;
+
+	for (int i=0; i < pServer->GetClientCount(); i++)
+	{
+		IClient *client = pServer->GetClient( i );
+		
+		if ( client->IsConnected() )
+		{
+			total++;
+
+			if ( client->IsActive() )
+				active++;
+			else
+				spawning++;
+		}
+	}
+
+	// create table
+	ILuaObject* ret = gLua->GetNewTable();
+	// set members
+	ret->SetMember( "active", (float)active );
+	ret->SetMember( "spawning", (float)spawning );
+	ret->SetMember( "total", (float)total );
+	// push to stack
+	gLua->Push( ret );
+	// cleanup
+	ret->UnReference();
+
+	return 1;
+}
+
+int Load( lua_State *L )
+{
+	gLua = Lua();
+
+#ifdef WIN32
+	CSigScan::sigscan_dllfunc = Sys_GetFactory( ENGINE_LIB );
+	
+	if ( CSigScan::GetDllMemInfo() )
+	{
+		CSigScan sigBaseServer;
+		sigBaseServer.Init( (unsigned char *)
+			"\x00\x00\x00\x00" "\x88\x1D\x00\x00\x00\x00" "\xE8\x00\x00\x00\x00" "\xD9\x1D",
+			"????xx????x????xx",
+			17 );
+
+		if ( sigBaseServer.is_set )
+			pServer = *(CBaseServer **)sigBaseServer.sig_addr;
+	}
+#else
+	void *hEngine = dlopen( ENGINE_LIB, RTLD_LAZY );
+
+	if ( hEngine )
+	{
+		pServer = (CBaseServer *)ResolveSymbol( hEngine, "sv" );
+
+		dlclose( hEngine );
+	}
+#endif
+
+	if ( !pServer )
+	{
+		gLua->Error( "Gatekeeper: failed to initialize (pServer is NULL)" );
+		
+		return 0;
+	}
+
+	HOOKVFUNC( pServer, (58 + VTABLE_OFFSET), origCheckPassword, newCheckPassword );
+	HOOKVFUNC( pServer, (49 + VTABLE_OFFSET), origConnectClient, newConnectClient );
+
+	// create table
+	ILuaObject *gatekeeper = gLua->GetNewTable();
+	// set members
+	gatekeeper->SetMember( "Drop", DropPlayer );
+	gatekeeper->SetMember( "GetNumClients", GetNumClients );
+	gatekeeper->SetMember( "DropAllClients", DropAllPlayers );
+	gatekeeper->SetMember( "GetUserByAddress", GetUserByAddress );
+	gatekeeper->SetMember( "ForceProtocol", ForceProtocol );
+	// register as global
+	ILuaObject *G = gLua->Global();
+	G->SetMember( "gatekeeper", gatekeeper );
+	// cleanup
+	gatekeeper->UnReference();
+
+	CreateInterfaceFn tier1Factory = VStdLib_GetICVarFactory();
+
+	g_pCVar = (ICvar *)tier1Factory( CVAR_INTERFACE_VERSION, NULL );
+
+	ConVar_Register();
+
+	return 0;
+}
+
+int Unload( lua_State *L )
+{
+	if ( !pServer )
+		return 0;
+
+	ConVar_Unregister();
+
+	UNHOOKVFUNC( pServer, (58 + VTABLE_OFFSET), origCheckPassword );
+	UNHOOKVFUNC( pServer, (49 + VTABLE_OFFSET), origConnectClient );
+
+	return 0;
+}
